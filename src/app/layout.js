bfb15129f41b3d58e85a47c7f4ef4b894343db94
asporQ <120199524+asporQ@users.noreply.github.com>
2023-08-07 15:59:21 +0700

    complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Tikomporn Luxsanaprokin"
+            studentId="650612085"
           />
         </div>
       </body>
